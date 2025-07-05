36b3a6e8b343e7a6c9fc0d272ef33c6a9b1cc6b6
YEBADOKPO <Sidoineko@users.noreply.huggingface.co>
2025-07-05 04:32:13 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Démarrer Streamlit en arrière-plan
-streamlit run src/streamlit_app.py --server.port=8501 --server.address=0.0.0.0 --server.fileWatcherType none --server.runOnSave false --server.headless true &
+streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0 --server.fileWatcherType none --server.runOnSave false --server.headless true &
 
-# Vérifier périodiquement que l'application est en cours d'exécution
+# Vérification simplifiée
 while true; do
-    # Vérifier si le serveur est en cours d'exécution
-    if ! pgrep -f "streamlit" > /dev/null; then
+    # Vérifier si le processus Streamlit est toujours en cours
+    if ! ps -p $! > /dev/null; then
         echo "Le serveur Streamlit a été arrêté. Redémarrage..."
-        streamlit run src/streamlit_app.py --server.port=8501 --server.address=0.0.0.0 --server.fileWatcherType none --server.runOnSave false --server.headless true &
+        streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0 --server.fileWatcherType none --server.runOnSave false --server.headless true &
     fi
     sleep 30
 done
