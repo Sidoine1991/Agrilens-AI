65f745aace11a78cddf66ac0843d02e237a0e445
YEBADOKPO <Sidoineko@users.noreply.huggingface.co>
2025-07-05 09:28:34 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
 
+#!/bin/bash
+cd /app
+streamlit run src/streamlit_app.py --server.port=8501 --server.address=0.0.0.0 --server.fileWatcherType none --server.runOnSave false --server.headless true
 # Démarrer Streamlit en arrière-plan
-streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0 --server.fileWatcherType none --server.runOnSave false --server.headless true &
 
 # Vérification simplifiée
 while true; do
